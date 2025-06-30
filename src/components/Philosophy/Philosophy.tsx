667f3011d092019e5bb80abe2e2f953cfbdbce55
Sammy <sammyanagolum@gmail.com>
2025-06-30 19:18:24 +0530

    Add real text for Philosophy section

--- src/components/Philosophy/Philosophy.tsx
+++ src/components/Philosophy/Philosophy.tsx
@@ -5,7 +5,7 @@ const Philosophy = () => {
                 Taking time to heal is not a luxury—it&apos;s a courageous act of self-respect and growth.
             </h2>
             <p className="font-freight-sans-pro-light mt-4 text-lg leading-loose">
-                You may be led to therapy by anxiety, depression, relationship stress, past or recent trauma, grief and loss, self-esteem issues, or challenges with family, parenting, or parental relationships. Whatever the source of your stress, you don&apos;t have to face it alone. Therapy offers you the time and space to work toward wellness and peace.
+                You might come to therapy for anxiety and stress, relationship challenges, trauma, grief, self-esteem challenges, or parenting concerns. Whatever brings you here, you don’t have to face it alone—therapy offers a compassionate space to build resilience and lasting well-being.
             </p>
         </section>
     )
