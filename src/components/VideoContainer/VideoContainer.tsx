cf0fe5518fa39f9daff45a938607266f6698170e
Sammy <sammyanagolum@gmail.com>
2025-06-30 17:49:54 +0530

    Add VideoContainer component for background video

---
+++ src/components/VideoContainer/VideoContainer.tsx
@@ -0,0 +1,18 @@
+const VideoContainer = () => {
+    return (
+        <div className="relative w-full h-full max-h-[95vh]">
+            <video
+                src={"https://dlwra9x5wdukt.cloudfront.net/background.mp4"}
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-full h-full object-cover object-center"
+            />
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-dark bg-opacity-30 pointer-events-none" />
+        </div>
+    );
+}
+
+export default VideoContainer;
