e4392b7de8f9e382bb7d4485be9567cf1a34f933
Sammy <sammyanagolum@gmail.com>
2025-06-30 22:01:21 +0530

    Fix alignment in Services component

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -6,11 +6,11 @@ import ServiceCard from "../ServiceCard/ServiceCard";
 const Services = () => {
     return (
         <section id="services" className="py-14 flex flex-col items-center text-center text-dark">
-            <p className="font-freight-display-pro-light text-[clamp(0px,3.7vw,45px)] mb-16">
+            <p className="font-freight-display-pro-light text-[clamp(0px,3.7vw,45px)] mb-20 mt-0.5">
                 Areas of Focus
             </p>
-            <ul className="flex flex-row items-start justify-center text-center gap-28 px-[4%]">
-                <li>
+            <ul className="flex flex-row items-start justify-center w-full text-center gap-14 px-[4%]">
+                <li className="flex items-center">
                     <ServiceCard
                         imgSrc={therapy1}
                         imgAlt=""
@@ -18,7 +18,7 @@ const Services = () => {
                         paraText="The persistent pull of worry, tension, and overwhelm can make even small tasks feel insurmountable. In our work together, we’ll explore the root causes of your anxiety—whether it’s performance pressure, life transitions, or chronic stress—and develop evidence-based tools to restore calm. Through a blend of cognitive-behavioral strategies, mindfulness exercises, and paced breathing techniques, you’ll learn to interrupt worry cycles, build resilience to daily pressures, and reclaim a sense of balance and ease in your life."
                     />
                 </li>
-                <li>
+                <li className="flex items-center">
                     <ServiceCard
                         imgSrc={therapy2}
                         imgAlt=""
@@ -26,7 +26,7 @@ const Services = () => {
                         paraText="Whether you’re navigating conflict with a partner, strengthening family bonds, or improving communication with friends, relationships shape our wellness in profound ways. Using empathic listening and structured dialogue, we’ll uncover patterns that contribute to misunderstandings and unmet needs. Together, we’ll practice healthy boundaries, assertive communication, and emotional attunement so you and your loved ones can foster deeper trust, mutual respect, and genuine connection."
                     />
                 </li>
-                <li>
+                <li className="flex items-center">
                     <ServiceCard
                         imgSrc={therapy3}
                         imgAlt=""
