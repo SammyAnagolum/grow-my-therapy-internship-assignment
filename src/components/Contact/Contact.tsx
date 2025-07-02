e559667e8a9bce98e79a83a34ec5865000b69065
Sammy <sammyanagolum@gmail.com>
2025-07-02 21:38:39 +0530

    Add fade-in animation to contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,6 +2,7 @@
 
 import ContactDetails from "../ContactDetails/ContactDetails";
 import ContactForm from "../ContactForm/ContactForm";
+import FadeInSection from "../FadeInSection/FadeInSection";
 
 const Contact = () => {
     return (
@@ -10,16 +11,24 @@ const Contact = () => {
             className="flex flex-col items-center text-dark w-full
             pb-14 px-[6vw] md:px-[4vw]"
         >
-            <h2 className="font-freight-display-pro-light text-[clamp(1.875rem,3.7vw,3rem)] mb-2">
-                Contact
-            </h2>
-            <p className="font-freight-display-pro-light text-center text-[clamp(1.5rem,2.2vw,2.2rem)] sm:text-[clamp(1.6rem,3vw,2.4rem)] text-muted max-w-xl ">
-                Take the first step toward your healing!
-            </p>
-            <div className="flex flex-col items-center lg:flex-row lg:items-start w-full justify-center gap-8 md:gap-12 lg:pt-8">
-                <ContactDetails />
-                <ContactForm />
-            </div>
+            <FadeInSection>
+                <h2 className="font-freight-display-pro-light text-[clamp(1.875rem,3.7vw,3rem)] mb-2">
+                    Contact
+                </h2>
+            </FadeInSection >
+            <FadeInSection delay={200}>
+                <p className="font-freight-display-pro-light text-center text-[clamp(1.5rem,2.2vw,2.2rem)] sm:text-[clamp(1.6rem,3vw,2.4rem)] text-muted max-w-xl ">
+                    Take the first step toward your healing!
+                </p>
+            </FadeInSection >
+            <FadeInSection delay={300}>
+                <div className="flex flex-col items-center lg:flex-row lg:items-start w-full justify-center gap-8 md:gap-12 lg:pt-8">
+                    <ContactDetails />
+                    <FadeInSection delay={400}>
+                        <ContactForm />
+                    </FadeInSection>
+                </div>
+            </FadeInSection >
 
         </section >
     );
