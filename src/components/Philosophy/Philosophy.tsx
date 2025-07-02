6da5c9bbd840ba9aff2de9d0e86f62f7b4123e66
Sammy <sammyanagolum@gmail.com>
2025-07-02 19:04:27 +0530

    Add responsiveness for Philosophy section

--- src/components/Philosophy/Philosophy.tsx
+++ src/components/Philosophy/Philosophy.tsx
@@ -1,10 +1,10 @@
 const Philosophy = () => {
     return (
-        <section id="philosophy" className="pt-12 pb-6 flex flex-col items-center text-dark text-center">
-            <h2 className="font-freight-display-pro-light text-[clamp(0px,3.7vw,48px)] mb-4 leading-tight">
+        <section id="philosophy" className="min-h-[400px] md:min-h-fit px-[6vw] md:px-[18vw] pt-5 sm:pt-8 lg:pt-10 xl:pt-12 pb-6 flex flex-col items-center text-dark text-center 2k:container 2k:mx-auto">
+            <h2 className="font-freight-display-pro-light text-[clamp(1.875rem,3.7vw,3rem)] mb-4 leading-tight">
                 Taking time to heal is not a luxury—it&apos;s a courageous act of self-respect and growth.
             </h2>
-            <p className="font-freight-sans-pro-light mt-4 text-lg leading-loose">
+            <p className="font-freight-sans-pro-light mt-4 text-md sm:text-lg leading-loose md:leading-8">
                 You might come to therapy for anxiety and stress, relationship challenges, trauma, grief, self-esteem challenges, or parenting concerns. Whatever brings you here, you don’t have to face it alone—therapy offers a compassionate space to build resilience and lasting well-being.
             </p>
         </section>
